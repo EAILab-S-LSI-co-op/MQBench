22b856f9294e410152ae0cfee91357c47a7b7d7b
ijgit <ijeong@unist.ac.kr>
2024-07-31 07:06:13 +0000

    update example

--- example/ptq/experiments.sh
+++ example/ptq/experiments.sh
@@ -1,3 +1,4 @@
-CUDA_VISIBLE_DEVICES=1 python3 ptq.py --config=/workspace/MQBench/example/advanced_ptq/configs/ptq-adaround.yaml > adaround.out
-CUDA_VISIBLE_DEVICES=1 python3 ptq.py --config=/workspace/MQBench/example/advanced_ptq/configs/ptq-brecq.yaml > brecq.out
-CUDA_VISIBLE_DEVICES=1 python3 ptq.py --config=/workspace/MQBench/example/advanced_ptq/configs/ptq-qdrop.yaml > qdrop.out
+CUDA_VISIBLE_DEVICES=1 python3 ptq.py --config=/workspace/MQBench/example/ptq/configs/ptq-adaround.yaml > adaround.out
+CUDA_VISIBLE_DEVICES=1 python3 ptq.py --config=/workspace/MQBench/example/ptq/configs/ptq-brecq.yaml > brecq.out
+CUDA_VISIBLE_DEVICES=1 python3 ptq.py --config=/workspace/MQBench/example/ptq/configs/ptq-qdrop.yaml > qdrop.out
+CUDA_VISIBLE_DEVICES=1 python3 ptq.py --config=/workspace/MQBench/example/ptq/configs/ptq-pact.yaml > pact.out
